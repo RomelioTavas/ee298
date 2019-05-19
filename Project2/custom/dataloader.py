4b7cba2f43dbe2b2f9815150ec67916b487018d5
Romelio P. Tavas Jr <romeliotavas@gmail.com>
2019-05-19 17:33:32 +0800

    Use batch loader in testing

--- Project2/custom/dataloader.py
+++ Project2/custom/dataloader.py
@@ -30,7 +30,7 @@ class DataLoader():
         return data
 
     def load_batch(self, batch_size=1):
-        path_A = glob('./{}/voi/*.wav'.format(self.dataset_name))
+        path_A = glob('./{}/pia/*.wav'.format(self.dataset_name))
         path_B = glob('./{}/gac/*.wav'.format(self.dataset_name))
 
         self.n_batches = int(min(len(path_A), len(path_B)) / batch_size)
@@ -65,6 +65,43 @@ class DataLoader():
                 audios_B[i, ] = audio_B
 
             yield audios_A, audios_B
+
+    def load_batch2(self, batch_size=1):
+        path_A = glob('./{}/pia/*.wav'.format(self.dataset_name))
+        path_B = glob('./{}/gac/*.wav'.format(self.dataset_name))
+
+        self.n_batches = int(min(len(path_A), len(path_B)) / batch_size)
+        total_samples = self.n_batches * batch_size
+
+        # Sample n_batches * batch_size from each path list so that model sees all
+        # samples from both domains
+        path_A = np.random.choice(path_A, total_samples, replace=False)
+        path_B = np.random.choice(path_B, total_samples, replace=False)
+
+        input_length = self.config.audio_length
+
+        for i in range(self.n_batches-1):
+            batch_A = path_A[i*batch_size:(i+1)*batch_size]
+            batch_B = path_B[i*batch_size:(i+1)*batch_size]
+            audios_A = np.empty((batch_size, input_length, 1))
+            audios_B = np.empty((batch_size, input_length, 1))
+
+            for i, (audio_A_path, audio_B_path) in enumerate(zip(batch_A, batch_B)):
+                audio_A, _ = librosa.core.load(audio_A_path, sr=self.config.sampling_rate,
+                                               res_type='kaiser_fast')
+                audio_B, _ = librosa.core.load(audio_B_path, sr=self.config.sampling_rate,
+                                               res_type='kaiser_fast')
+
+                audio_A = self.clip_audio(audio_A)
+                audio_B = self.clip_audio(audio_B)
+
+                audio_A = self.normalize_audio(audio_A)[:, np.newaxis]
+                audio_B = self.normalize_audio(audio_B)[:, np.newaxis]
+
+                audios_A[i, ] = audio_A
+                audios_B[i, ] = audio_B
+
+            yield audios_A, audios_B, audio_A_path, audio_B_path
 
     def load(self, path):
         input_length = self.config.audio_length
--- Project2/test.py
+++ Project2/test.py
@@ -12,12 +12,13 @@ if __name__ == '__main__':
 
     cycleGAN = CycleGAN(config, data_loader)
 
-    source_path = "IRMAS-TrainingData/voi"
-    input_sample = data_loader.load(source_path + "/[voi][jaz_blu]2491__1.wav")
-
     g_AB = cycleGAN.build_generator()
     g_AB.load_weights("checkpoints/in_16khz/cyclegan_A2B_epoch_100.h5")
-    output_sample = g_AB.predict(input_sample)
 
-    librosa.output.write_wav("output.wav", output_sample[0], config.sampling_rate)
-    print("Written output.wav")
+    for i, (audios_A, _, audio_A_path, _) in enumerate(data_loader.load_batch2(1)):
+
+        output_sample = g_AB.predict(audios_A)
+        output_filename = audio_A_path.split('/')[-1]
+        output_path = "generated/converted-{}".format(output_filename)
+        librosa.output.write_wav(output_path, output_sample[0], config.sampling_rate)
+        print("Written {}".format(output_path))
